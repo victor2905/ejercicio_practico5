dd97d9cd7639efa315c35b916e6ec2272f36b69f
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:31:21 -0500

    ejercicio_practico5
    Creación de proyecto

---
+++ ejercicio_practico5.py
@@ -0,0 +1,10 @@
+print("******************")
+print("Serie de fibonacci")
+print("******************")
+A = 0
+x = 1
+while A <= 1597:
+    print(A,x,end=" ")
+    A = A + x
+    x = A + x
+print("Fin.")
